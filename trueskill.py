711febf2bc0d45f609613ee110f3a410bc297482
Rob Speer <rspeer@mit.edu>
2010-11-22 03:14:26 -0500

    do some tweaking, add csv

--- trueskill.py
+++ trueskill.py
@@ -2,12 +2,14 @@
 from __future__ import print_function
 import math
 import sys
+import csv
 
 from scipy.stats.distributions import norm as scipy_norm
+import numpy as np
 
-beta = 25./6
-gamma = 25./300
-epsilon = 0.08
+beta = 24.
+gamma = 1./12
+epsilon = 0.01
 
 norm = scipy_norm()
 
@@ -41,42 +43,67 @@ def true_skill(winner, loser):
     
     return winner, loser
 
-def update(squads, winner, loser):
-    winner_stats = squads.get(winner, (25, 25/3))
-    loser_stats = squads.get(loser, (25, 25/3))
-    squads[winner], squads[loser] = true_skill(winner_stats, loser_stats)
+def update(player_ranks, winner, loser):
+    winner_stats = player_ranks.get(winner, (25, 25./3))
+    loser_stats = player_ranks.get(loser, (25, 25./3))
+    player_ranks[winner], player_ranks[loser] = true_skill(winner_stats, loser_stats)
 
-def rank(squads, squad):
-    return squads[squad][0] - 3*squads[squad][1]
+def rank(player_ranks, player):
+    return player_ranks[player][0] - 3*player_ranks[player][1]
 
+import random
 def main(argv):
-    games = [
-        ("PK", "SF")   , ("RT", "XT")  , ("ACE", "BS") , ("SRM", "GB") , #day 1
-        ("SysX", "XT") , ("PK", "RT")  , ("SF", "GB")  , ("SRM", "BS") , #day 2
-        ("SysX", "GB") , ("RT", "BS")  , ("SF", "SRM") , ("XT", "ACE") , #day 3
-        ("SysX", "PK") , ("RT", "SF")  , ("XT", "BS")  , ("GB", "SRM") , #day 4
-        ("SysX", "RT") , ("PK", "ACE") , ("XT", "SF")  , ("BS", "GB")  , #day 5
-        ("SysX", "SF") , ("RT", "GB")  , ("XT", "PK")  , ("ACE", "SRM"), #day 6
-        ("SysX", "ACE"), ("RT", "SF")  , ("XT", "PK")  , ("BS", "SRM") , #day 7
-        ("SysX", "BS") , ("PK", "GB")  , ("RT", "SRM") , ("ACE", "SF") , #day 8
-        ("SysX", "RT") , ("PK", "SRM") , ("XT", "SF")  , ("BS", "ACE") , #day 9
-        ("PK", "SF")   , ("XT", "SysX"), ("SRM", "ACE"), ("BS", "GB")  , #day 10
-        ("SysX", "SF") , ("PK", "RT")  , ("XT", "SRM") , ("ACE", "GB") , #day 11
-        ("SysX", "SRM"), ("PK", "BS")  , ("RT", "ACE") , ("XT", "GB")  , #day 12
-        ("SysX", "PK") , ("RT", "XT")  , ("BS", "ACE") , ("GB", "ACE") ] #day 13
+    games = []
+    reader = csv.reader(open(argv[1]))
+    for row in reader:
+        scorelist = row[6:]
+        assert len(scorelist) % 4 == 0
+        players = []
+        for playernum in xrange(len(scorelist) // 4):
+            name, win, pts, turns = scorelist[playernum*4 : (playernum+1)*4]
+            players.append((name, int(pts), int(turns)))
+        
+        # fixme: this awards ties to later players, which isn't actually
+        # correct
+        players.sort(key = lambda x: (x[1], -x[2]))
+        for loser in xrange(len(players) - 1):
+            for winner in xrange(loser + 1, len(players)):
+                games.append( (players[winner][0], players[loser][0]) )
 
-    squads = {}
+    player_ranks = {}
     
     for winner, loser in games:
-        update(squads, winner, loser)
+        update(player_ranks, winner, loser)
     
-    for squad in sorted(squads, key=lambda squad: rank(squads, squad)):
-        mu = squads[squad][0]
-        sigma = squads[squad][1]
-        print("%6s R: %5.2f mu: %5.2f sigma: %5.2f" % (squad, rank(squads, squad), mu, sigma))
+    mus = []
+    histogram = np.zeros((50,), 'i')
+    for player in sorted(player_ranks, key=lambda player: rank(player_ranks, player)):
+        mu = player_ranks[player][0]
+        sigma = player_ranks[player][1]
+        print("%20s R: %5.2f mu: %5.2f sigma: %5.2f" % (player, rank(player_ranks, player), mu, sigma))
+        mus.append(mu)
+        bin = int(max(0, np.floor(rank(player_ranks, player))))
+        histogram[bin] += 1
     
-    return 0
+    print((np.mean(mus), np.std(mus)))
+    print(histogram)
+
+    win_levels = np.zeros((100,))
+    loss_levels = np.zeros((100,))
+    for winner, loser in games:
+        upset = False
+        diff = rank(player_ranks, winner) - rank(player_ranks, loser)
+        if diff < 0:
+            upset = True
+            diff = -diff
+        if upset:
+            loss_levels[int(diff)] += 1
+        else:
+            win_levels[int(diff)] += 1
+    probs = win_levels / (win_levels + loss_levels)
+    print(probs)
+        
+    return player_ranks, probs
 
 if __name__ == "__main__":
-    exit_code = main(sys.argv)
-    sys.exit(exit_code)
+    main(sys.argv)
